5d4af0c50cf63dc3d5700c22b4f954bbf95109af
Roushan ali <roushanrhaman064@gmail.com>
2023-12-05 13:08:50 +0600

    academic faculty controller done

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,66 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utilitys/catchAsync";
+import sendResponse from "../../utilitys/sendResponse";
+import { academicFacultyServices } from "./academicFaculty.services";
+
+// create academic faculty
+const createAcademicFaculty = catchAsync(async (req, res)=>{
+
+    const result = await academicFacultyServices.createAcademicFacultyInToDB(req.body)
+    
+    sendResponse(res,{
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "academic faculty is create successfully",
+        data: result
+    })
+})
+
+// get academic faculty
+const getAllAcademicFaculty = catchAsync(async (req,res)=>{
+    const result = await academicFacultyServices.getAllAcademicFacultyFromDb()
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'all academic faculty retrieve successfully',
+        data: result
+    })
+})
+
+// get single academic faculty 
+const geaSingleAcademicFaculty = catchAsync(async (req,res)=>{
+    
+    const {id} = req.params
+
+    const result = await academicFacultyServices.singleAcademicFacultyFromDb(id)
+
+    sendResponse(res,{
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'academic faculty retrieve successFully',
+        data: result
+    })
+});
+
+// update academic faculty
+const updateAcademicFaculty = catchAsync(async (req,res)=>{
+    const {id} = req.params;
+
+    const result = await academicFacultyServices.updateAcademicFacultyFromDB(id,req.body)
+
+    sendResponse(res,{
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'academic faculty update successfully',
+        data: result
+    })
+})
+
+
+export const academicFacultyController = {
+    createAcademicFaculty,
+    getAllAcademicFaculty,
+    geaSingleAcademicFaculty,
+    updateAcademicFaculty
+}
