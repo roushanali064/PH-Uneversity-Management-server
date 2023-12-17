90b0f92c10836f983473d796aac6e744dadde584
Roushan ali <roushanrhaman064@gmail.com>
2023-12-17 15:57:18 +0600

    model and semester registration validation done

--- src/app/modules/semesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -1,4 +1,5 @@
 import z from 'zod';
+import { semesterRegistrationStatus } from './semesterRegistration.conostant';
 
 // create semester registration validation schema
 const createSemesterRegistrationValidationSchema = z.object({
@@ -10,7 +11,12 @@ const createSemesterRegistrationValidationSchema = z.object({
 // update semester registration validation schema
 const updateSemesterRegistrationValidationSchema = z.object({
     body: z.object({
-        name: z.string()
+        academicSemester: z.string(),
+        status: z.enum([...semesterRegistrationStatus] as [string, ...string[]]),
+        startDate: z.string().datetime(),
+        endDate: z.string().datetime(),
+        minCredit: z.number(),
+        maxCredit: z.number()
     })
 })
 
