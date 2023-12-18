c0039b2dc6bd1cfd50b81ef33900068ec1a2e71d
Roushan ali <roushanrhaman064@gmail.com>
2023-12-18 13:45:16 +0600

    offered course model [done]

---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+import { TOfferedCourse } from "./offeredCourse.interface";
+
+const offeredCourseSchema = new Schema<TOfferedCourse>({
+    semesterRegistration: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'SemesterRegistration'
+    },
+    academicSemester: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'AcademicSemester'
+    },
+    academicFaculty: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'academicFaculty'
+    },
+    academicDepartment: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'AcademicDepartMent'
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Courses'
+    },
+    faculty: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Faculty'
+    },
+    maxCapacity: {
+        type: Number,
+        required: true
+    },
+    section: {
+        type: Number,
+        required: true
+    },
+    days: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    }
+})
+
+
+export const OfferedCourseModel = model<TOfferedCourse>('OfferedCourse', offeredCourseSchema)
