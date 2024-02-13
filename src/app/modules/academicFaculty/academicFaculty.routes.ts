086ad45c74c1b0bf57ab42b5049a592df5cc85cf
Roushan ali <roushanrhaman064@gmail.com>
2024-02-13 17:18:39 +0600

    academic semester and faculti refactroing

--- src/app/modules/academicFaculty/academicFaculty.routes.ts
+++ src/app/modules/academicFaculty/academicFaculty.routes.ts
@@ -1,20 +1,39 @@
-import express from 'express'
+import express from 'express';
 import { academicFacultyController } from './academicFaculty.controller';
 import requestValidation from '../../midleware/requestValidation';
 import { academicFacultyValidation } from './academicFaculty.validation';
+import auth from '../../midleware/auth';
+import { userRole } from '../user/user.constant';
 
 const router = express.Router();
 
 // create academic faculty route
-router.post('/create-academic-faculty',requestValidation(academicFacultyValidation.academicFacultyValidationSchema),academicFacultyController.createAcademicFaculty)
+router.post(
+  '/create-academic-faculty',
+  auth(userRole.superAdmin, userRole.admin),
+  requestValidation(academicFacultyValidation.academicFacultyValidationSchema),
+  academicFacultyController.createAcademicFaculty,
+);
 
 // get all academic faculty route
-router.get('/',academicFacultyController.getAllAcademicFaculty)
+router.get(
+  '/',
+  auth(userRole.superAdmin, userRole.admin),
+  academicFacultyController.getAllAcademicFaculty,
+);
 
 // get single academic faculty
-router.get('/:facultyId', academicFacultyController.geaSingleAcademicFaculty)
+router.get(
+  '/:facultyId',
+  auth(userRole.superAdmin, userRole.admin),
+  academicFacultyController.geaSingleAcademicFaculty,
+);
 
 // update academic faculty
-router.put('/:facultyId',requestValidation(academicFacultyValidation.academicFacultyValidationSchema), academicFacultyController.updateAcademicFaculty)
+router.put(
+  '/:facultyId',
+  requestValidation(academicFacultyValidation.academicFacultyValidationSchema),
+  academicFacultyController.updateAcademicFaculty,
+);
 
-export const academicFacultyRoute = router
+export const academicFacultyRoute = router;
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -3,15 +3,35 @@ import { academicSemesterController } from './academicSemester.controller';
 import requestValidation from '../../midleware/requestValidation';
 import { academicSemesterValidation } from './academicSemester.validation';
 import auth from '../../midleware/auth';
+import { userRole } from '../user/user.constant';
 
 const router = express.Router();
 
-router.post('/create-academic-semester',auth('admin'),requestValidation(academicSemesterValidation.createAcademicSemesterValidation), academicSemesterController.createAcademicSemester);
+router.post(
+  '/create-academic-semester',
+  auth(userRole.superAdmin, userRole.admin),
+  requestValidation(
+    academicSemesterValidation.createAcademicSemesterValidation,
+  ),
+  academicSemesterController.createAcademicSemester,
+);
 
-router.get('/',auth('admin','faculty'), academicSemesterController.getAllAcademicSemester)
+router.get(
+  '/',
+  auth(userRole.superAdmin, userRole.admin, userRole.faculty),
+  academicSemesterController.getAllAcademicSemester,
+);
 
-router.get('/:semesterId',auth('admin','faculty'), academicSemesterController.singleAcademicSemester)
+router.get(
+  '/:semesterId',
+  auth(userRole.superAdmin, userRole.admin, userRole.faculty),
+  academicSemesterController.singleAcademicSemester,
+);
 
-router.patch('/:semesterId',auth('admin','faculty'), academicSemesterController.updateAcademicSemester)
+router.patch(
+  '/:semesterId',
+  auth(userRole.superAdmin, userRole.admin),
+  academicSemesterController.updateAcademicSemester,
+);
 
-export const academicSemesterRoute = router
+export const academicSemesterRoute = router;
